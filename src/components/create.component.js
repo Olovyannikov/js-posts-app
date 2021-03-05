f17bf66a14a0b8231be316ab02385f766257f509
Илья Оловянников <ilaolovannikov@iMac-Ila.local>
2021-03-05 16:56:47 +0300

    Добавил метод isValid

--- src/components/create.component.js
+++ src/components/create.component.js
@@ -1,7 +1,28 @@
 import {Component} from "../core/component";
+import {Form} from "../core/form";
+import {Validators} from "../core/validators";
 
 export class CreateComponent extends Component {
     constructor(id) {
         super(id);
+    }
+
+    init() {
+        this.$el.addEventListener('submit', submitHandler.bind(this));
+        this.form = new Form(this.$el, {
+            title: [Validators.required],
+            fulltext: [Validators.required]
+        });
+    }
+}
+
+function submitHandler(e) {
+    e.preventDefault();
+
+    if (this.form.isValid()) {
+        const formData = {
+            type: this.$el.type.value,
+            ...this.form.value()
+        }
     }
 }
